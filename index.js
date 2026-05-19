30a658085a9706af85fbf561faadbc58becfe6d8
Naim Uddin <naimxofficial.bd@gmail.com>
2026-05-20 01:40:11 +0600

    npm installation , emv and index setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+dotenv.config()
+const uri = process.env.MONGODB_URI
+
+const app = express()
+const PORT = process.env.PORT
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
